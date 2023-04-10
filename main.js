bc67b3f1b06df6fb2c1f8105f3c3a326bdf28087
Ben Buckley <BenMBuckley@gmail.com>
2023-04-10 18:48:22 -0300

    works for multiple pages of static search

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const PORT = 8787
 
 const app = express()
 
-const url = 'https://www.yellowpages.ca/search/si/1/plumber/Halifax+NS'
+let searchTerm = 'plumber'
+let searchLocation = 'Halifax+NS'
+let currentPage = 1
+
+const url = `https://www.yellowpages.ca/search/si/1/${searchTerm}/${searchLocation}`
 
 {/* <li class="mlr__item mlr__item--more mlr__item--phone jsMapBubblePhone">
 <a href="javascript:void(0)" class="mlr__item__cta jsMlrMenu" title="Get the Phone Number" data-analytics-placeholder="{&quot;lk_listing_id&quot;:&quot;2137994&quot;,&quot;lk_non-ad-rollup&quot;:&quot;0&quot;,&quot;lk_page_num&quot;:&quot;1&quot;,&quot;lk_pos&quot;:&quot;in_listing&quot;,&quot;lk_directory_heading&quot;:[{&quot;091300&quot;:[{&quot;01030400&quot;:&quot;1&quot;}]}],&quot;lk_geo_tier&quot;:&quot;dir&quot;,&quot;lk_area&quot;:&quot;left_1&quot;,&quot;lk_relevancy&quot;:&quot;1&quot;,&quot;lk_name&quot;:&quot;revealphonenumber&quot;,&quot;lk_pos_num&quot;:&quot;12&quot;,&quot;lk_se_id&quot;:&quot;67137c47-9e11-453b-b315-596f231b466c_cGx1bWJlcg_SGFsaWZheCBOUw&quot;,&quot;lk_ev&quot;:&quot;link&quot;,&quot;event_name&quot;:&quot;click - Call - Reveal Phone Number&quot;,&quot;lk_product&quot;:&quot;l1&quot;}" data-phone="902-453-4800">
@@ -22,11 +26,19 @@ fetch(url)
     //console.log(html)
     const articles = []
     const $ = cheerio.load(html)
+    const pageCountText = $('.pageCount').find('span:nth-child(2)').text()
+    const pageCountNum = parseInt(pageCountText)
     $('.listing_right_section', html).each(function(){
-        //const title = $(this).text()
-        //const link = $(this).find('a').attr('data-phone')
-        const businessName = $(this).find('h3').text()
-        // const address = $(this).find('span').attr('')
+        const extraNbusinessName = $(this).find('h3').text()
+        const arr = extraNbusinessName.split('')
+        arr.shift()
+        arr.pop()
+        if(arr[1]==='\n')
+        {
+            arr.shift()
+            arr.shift()
+        }
+        const businessName = arr.join('')
         const streetAddress = $(this).find('[itemprop="streetAddress"]').text()
         const city = $(this).find('[itemprop="addressLocality"]').text()
         const provinceCode = $(this).find('[itemprop="addressRegion"]').text()
@@ -40,9 +52,52 @@ fetch(url)
             postalCode,
             phoneNumber
         })
-        console.log(articles)
-    }).catch(err => console.log(err))
-})
+    })
+    if(pageCountNum>1)
+    {
+        for(let i=2;i<=pageCountNum;i++)
+        {
+            currentPage = i
+            const loopUrl = `https://www.yellowpages.ca/search/si/${currentPage}/${searchTerm}/${searchLocation}`
+            console.log('This is loop: ' + i)
+                getNextPage(loopUrl)
+                async function getNextPage(currentUrl){
+                    const response = await fetch(currentUrl)
+                    const html = await response.text()
+                    console.log('This is fetch:'+ i)
+                    let currentFetch = i
+                    const $ = cheerio.load(html)
+                    const pageCountText = $('.pageCount').find('span:nth-child(2)').text()
+                    const pageCountNum = parseInt(pageCountText)
+                    $('.listing_right_section', html).each(function(){
+                        const extraNbusinessName = $(this).find('h3').text()
+                        const arr = extraNbusinessName.split('')
+                        arr.shift()
+                        arr.pop()
+                        const businessName = arr.join('')
+                        const streetAddress = $(this).find('[itemprop="streetAddress"]').text()
+                        const city = $(this).find('[itemprop="addressLocality"]').text()
+                        const provinceCode = $(this).find('[itemprop="addressRegion"]').text()
+                        const postalCode = $(this).find('[itemprop="postalCode"]').text()
+                        const phoneNumber = $(this).find('h4').text()
+                        articles.push({
+                            businessName,
+                            streetAddress,
+                            city,
+                            provinceCode,
+                            postalCode,
+                            phoneNumber
+                        })
+                    })
+                    if(currentFetch>6)
+                    {
+                        console.table(articles)
+                    }
+                }
+            }
+        }
+        // console.table(articles)
+    })
 
 app.listen(PORT, ()=>console.log(`Server is alive on ${PORT}`))
 
