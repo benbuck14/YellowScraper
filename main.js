2db627a7598081e566912267324d80778986dabd
Ben Buckley <BenMBuckley@gmail.com>
2023-04-12 18:32:21 -0300

    Cleaned up and added comments

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const PORT = 8787
 
 const app = express()
 
+//searchTerm & searchLocation to be entered by user
+
 let searchTerm = 'plumber'
 let searchLocation = 'Halifax+NS'
 let currentPage = 1
@@ -15,28 +17,32 @@ fetch(url)
 .then(response=>response.text())
 .then(html=>{
     //console.log(html)
-    const articles = []
     const firstFetchArray = []
+    //cheerio is a module to parse and manipulate html 
+    // $ is conventional reference to html doc from cheerio
     const $ = cheerio.load(html)
     const pageCountText = $('.pageCount').find('span:nth-child(2)').text()
     const pageCountNum = parseInt(pageCountText)
+    //following function steps through each search result (CSS class)
     $('.listing_right_section', html).each(function(){
         const extraNbusinessName = $(this).find('h3').text()
-        const arr = extraNbusinessName.split('')
-        arr.shift()
-        arr.pop()
-        if(arr[1]==='\n')
+        //this result has extra characters, the following code removes them
+        const arrayToRemoveCharacters = extraNbusinessName.split('')
+        arrayToRemoveCharacters.shift()
+        arrayToRemoveCharacters.pop()
+        if(arrayToRemoveCharacters[1]==='\n')
         {
-            arr.shift()
-            arr.shift()
+            arrayToRemoveCharacters.shift()
+            arrayToRemoveCharacters.shift()
         }
-        const businessName = arr.join('')
+        const businessName = arrayToRemoveCharacters.join('')
         const streetAddress = $(this).find('[itemprop="streetAddress"]').text()
         const city = $(this).find('[itemprop="addressLocality"]').text()
         const provinceCode = $(this).find('[itemprop="addressRegion"]').text()
         const postalCode = $(this).find('[itemprop="postalCode"]').text()
         const phoneNumber = $(this).find('h4').text()
         const _id = phoneNumber
+        //relevant data for first page captured above.  Push to array and update DB below
         firstFetchArray.push({
             _id,
             businessName,
@@ -44,10 +50,12 @@ fetch(url)
             city,
             provinceCode,
             postalCode,
-            phoneNumber
+            phoneNumber,
+            searchTerm
         })
     })
     //db.collection.updateMany(_id,firstFetchArray,upsert: true)
+    //If more than one page of results, loop through each page and repeat the above steps
     if(pageCountNum>1)
     {
         for(let i=2;i<=pageCountNum;i++)
@@ -84,7 +92,8 @@ fetch(url)
                             city,
                             provinceCode,
                             postalCode,
-                            phoneNumber
+                            phoneNumber,
+                            searchTerm
                         })
                     })
                     //db.collection.updateMany(_id,loopFetchArray,upsert: true)
